5a1b68bd6325fb5678968d899325356054716897
inkdust-dev <ink-dust@outlook.com>
2022-09-01 13:40:49 +0000

    vercel php2

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 $feed_url = "https://blog.inkdust.top/atom.xml";
-$data = simplexml_load_file($feed_url)->channel->item;
+$data = simplexml_load_file("https://blog.inkdust.top/atom.xml")->channel->item;
 $resp = [
     "status" => 0,
     "data" => []
